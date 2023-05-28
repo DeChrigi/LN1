f9b53e2b1733d5865929c6bb5bd40d9721a7c3f8
DeChrigi <schweizerchristian50@gmail.com>
2023-05-28 12:06:40 +0200

    Löschung der unnötigen Objekte

--- test.py
+++
@@ -1,9 +0,0 @@
-import db_access
-import pandas as pd 
-
-df = db_access.get_top_songs()
-df_bar = df.assign(Genres=df['Genres'].str.split(', ')).explode('Genres')
-
-df_group_by = df_bar.groupby(['Genres']).agg({'Tempo': 'mean'})
-print(type(df_group_by))
-
